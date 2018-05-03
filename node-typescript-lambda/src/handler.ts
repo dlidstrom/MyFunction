a044de0bad12d0d879924edb7a03ce3beaef3099
Daniel Lidstrom <dlidstrom@gmail.com>
2018-05-03 07:04:03 +0200

    Lambda TypeScript

---
+++ node-typescript-lambda/src/handler.ts
@@ -0,0 +1,22 @@
+import { Handler, Context, Callback } from 'aws-lambda';
+
+interface HelloResponse {
+    statusCode: number;
+    body: string;
+}
+
+const hello: Handler = (event, context: Context, callback: Callback) => {
+    const response: HelloResponse = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: Math.floor(Math.random() * 10)
+        })
+    };
+
+    callback(null, response);
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+export { hello }
